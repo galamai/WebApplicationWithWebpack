8f79f43e3486fa941426492a3053202753cc7f43
Ievgen Galamai <galamayevg@gmail.com>
2018-02-03 08:07:54 +0200

    ASP.NET MVC5 Setup Webpack

---
+++ WebApplicationWithWebpack/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './Client/src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'Client/dist')
+    }
+};
